20ffa034526d96d74aeda59dc7b9bb376bfd05b3
Edgar Mancillas <eman@skupee.online>
2023-03-16 14:27:48 -0600

    fix: Newsletter code issue

---
+++ src/components/custom/Input/Loader.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss'
+
+const Loader = () => {
+    return (
+        <div><div className={styles.ldsRipple}><div></div><div></div></div></div>
+    )
+}
+
+export default Loader
