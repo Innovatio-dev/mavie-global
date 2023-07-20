8962740235b37b10ffb0279aeaae470c85f3f91a
Christian Sanchez <christian.s.molina@gmail.com>
2023-07-20 17:05:17 -0600

    Fixing some bugs

--- src/components/custom/ScoreBoard/index.tsx
+++ src/components/custom/ScoreBoard/index.tsx
@@ -15,17 +15,17 @@ export default function ScoreBoard() {
                     {
                         SCORE_DATA.map(({ ranking, company, revenue }) => (
                             <div key={ranking} className={`grid grid-cols-[110px_1fr_auto] items-center ${company === 'MaVie Global' ? 'bg-main-gradient text-black' : 'bg-brand-black'}`}>
-                                <td className={`py-[14.13px] pl-[26px] ${ranking === 10 && 'rounded-bl-xl overflow-hidden'}`}>
+                                <div className={`py-[14.13px] pl-[26px] ${ranking === 10 && 'rounded-bl-xl overflow-hidden'}`}>
                                     <div className={'w-[clamp(45px,_1vw,_48px)] h-[clamp(45px,_1vw,_48px)] flex justify-end bg-vertical-gradient rounded'}>
                                         <div className="w-[calc(100%-0.2rem)] h-full flex flex-col justify-center items-center bg-black rounded-r">
                                             <span className="text-[clamp(20px,_1vw,_30px)] text-white font-eesti font-semibold italic uppercase tracking-[2.6px] !leading-none -ml-[0.2rem]">{ ranking }</span>
                                         </div>
                                     </div>
-                                </td>
-                                <td className='py-[14.13px]'>{company}</td>
-                                <td className={`py-[14.13px] pr-[26px] ${ranking === 10 && 'rounded-br-xl overflow-hidden'}`}>
+                                </div>
+                                <div className='py-[14.13px]'>{company}</div>
+                                <div className={`py-[14.13px] pr-[26px] ${ranking === 10 && 'rounded-br-xl overflow-hidden'}`}>
                                     <div className='flex justify-between items-center'>{revenue} <IoChevronForwardOutline /></div>
-                                </td>
+                                </div>
                             </div>
                         ))
                     }
