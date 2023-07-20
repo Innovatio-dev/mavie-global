63bbd76498dd96793f79acd678dbf0ba9b46ac72
Ivan Tovar <info@innovatio.dev>
2023-07-20 12:38:12 -0600

    Table changed to CSS grid

--- src/components/custom/ScoreBoard/index.tsx
+++ src/components/custom/ScoreBoard/index.tsx
@@ -4,22 +4,17 @@ import { IoChevronForwardOutline } from 'react-icons/io5'
 export default function ScoreBoard() {
     return (
         <div className='w-full'>
-            <table className={`
-                w-full font-dm text-xl border-separate border-spacing-y-3
-                [&_th]:text-[clamp(12px,_1vw,_16px)]
-                [&_td]:text-[clamp(12px,_3vw,_16px)]
-            `}>
-                <thead>
-                    <tr className='bg-purple-gradient italic'>
-                        <th className='font-normal text-start rounded-tl-xl py-[14.13px] pl-[26px]'>NR</th>
-                        <th className='font-normal text-start py-[14.13px]'>COMPANY</th>
-                        <th className='font-normal text-start rounded-tr-xl py-[14.13px] pr-[26px]'>REVENUE <br /> GROWTH IN %</th>
-                    </tr>
-                </thead>
-                <tbody className='font-bold'>
+            <div className="w-full">
+                <div className='bg-purple-gradient italic grid grid-cols-[110px_1fr_auto] items-center'>
+                    <span className='font-normal text-start rounded-tl-xl py-[14.13px] pl-[26px]'>NR</span>
+                    <span className='font-normal text-start py-[14.13px]'>COMPANY</span>
+                    <span className='font-normal text-start rounded-tr-xl py-[14.13px] pr-[26px]'>REVENUE <br /> GROWTH IN %</span>
+                </div>
+
+                <div>
                     {
                         SCORE_DATA.map(({ ranking, company, revenue }) => (
-                            <tr key={ranking} className={`${company === 'MaVie Global' ? 'bg-main-gradient text-black' : 'bg-brand-black'}`}>
+                            <div key={ranking} className={`grid grid-cols-[110px_1fr_auto] items-center ${company === 'MaVie Global' ? 'bg-main-gradient text-black' : 'bg-brand-black'}`}>
                                 <td className={`py-[14.13px] pl-[26px] ${ranking === 10 && 'rounded-bl-xl overflow-hidden'}`}>
                                     <div className={'w-[clamp(45px,_1vw,_48px)] h-[clamp(45px,_1vw,_48px)] flex justify-end bg-vertical-gradient rounded'}>
                                         <div className="w-[calc(100%-0.2rem)] h-full flex flex-col justify-center items-center bg-black rounded-r">
@@ -31,11 +26,12 @@ export default function ScoreBoard() {
                                 <td className={`py-[14.13px] pr-[26px] ${ranking === 10 && 'rounded-br-xl overflow-hidden'}`}>
                                     <div className='flex justify-between items-center'>{revenue} <IoChevronForwardOutline /></div>
                                 </td>
-                            </tr>
+                            </div>
                         ))
                     }
-                </tbody>
-            </table>
+                </div>
+            </div>
+
             <span className='block font-dm text-sm text-brand-gray text-center mt-3'>(MaVie Global - the 2nd fastest revenue-growing affiliate company in the world)</span>
         </div>
 
