dd1babbfcd6d5d6cfdde09ab02ac3de81188332e
Ivan Tovar <info@innovatio.dev>
2023-11-20 11:36:18 -0600

    Tagalog added to the page About

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
     i18n: {
         defaultLocale: 'en',
-        locales: ['en', 'hu', 'jp', 'es', 'pt'],
+        locales: ['en', 'hu', 'jp', 'es', 'pt', 'ph'],
         localePath: path.resolve('./public/locales'),
         localeDetection: false
     }
